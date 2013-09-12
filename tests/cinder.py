69104a48f8b37662ffa302bc91f130d41a33c97a
Jakub Ruzicka <jruzicka@redhat.com>
2013-09-12 14:32:30 -0400

    Return basic cinder test.

---
+++ tests/cinder.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys
+
+if __name__ == '__main__':
+    import common.direct_testing as testing
+    testing.setup()
+
+from clismoke.sh import run
+from clismoke.core import fail
+from clismoke import log
+from clismoke.tester import run_tests_module
+
+import common.shared as shared
+
+
+TEST_VOLUME_NAME = 'test_volume_clismoke'
+TEST_SNAPSHOT_NAME = 'test_snapshot_clismoke'
+
+
+def test_basic():
+    o = run("cinder list | grep '%s'" % TEST_VOLUME_NAME, fatal=False)
+    if o.success:
+        log.info("Testing volume already present...")
+    else:
+        run("cinder create --display-name '%s' 1" % TEST_VOLUME_NAME)
+        shared.wait_for_output("cinder show '%s'" % TEST_VOLUME_NAME,
+                               'status.*active')
+    o = run("cinder show test_volume_clismoke")
+    volume_id = shared.parse_id(o)
+    o = run("cinder snapshot-create --display-name '%s' '%s'" % (TEST_SNAPSHOT_NAME, volume_id))
+    snapshot_id = shared.parse_id(o)
+    shared.wait_for_output("cinder snapshot-show '%s'" % snapshot_id,
+                           'status.*available')
+    run("cinder snapshot-list --volume-id '%s' | grep '%s'" % (volume_id, snapshot_id))
+    run("cinder snapshot-delete '%s'" % snapshot_id)
+    shared.wait_for_returncode("cinder snapshot-show '%s'" % snapshot_id, 1)
+    run("cinder delete '%s'" % volume_id)
+
+
+def test_version():
+    shared.test_version('cinder')
+
+def test_manpage():
+    shared.test_manpage('cinder')
+
+
+
+if __name__ == '__main__':
+    run_tests_module(sys.modules[__name__])
--- tests/common/shared.py
+++ tests/common/shared.py
@@ -18,6 +18,23 @@ def wait_for_output(command, pattern, timeout=30, period=1.0):
         log.info("%g s remaining..." % (timeout - t))
     fail("Timeout: Command didn't return desired output within %d s." % timeout)
 
+def wait_for_returncode(command, return_code, timeout=30, period=1.0):
+    t = 0.0
+    while t < timeout:
+        out = run(command, fatal=False)
+        if out.return_code == return_code:
+            log.info("Got desired return code.")
+            return
+        sleep(period)
+        t += period
+        log.info("%g s remaining..." % (timeout - t))
+    fail("Timeout: Command didn't return desired return code within %d s." % timeout)
+
+def parse_id(output):
+    m = re.search('^\|\s+id\s+\|\s(\S+)\s\|', output, flags=re.M)
+    if not m:
+        raise fail("Failed to parse ID from command output.")
+    return m.group(1)
 
 def test_version(cli, prefix=''):
     ver = run('%s --version 2>&1' % cli)
